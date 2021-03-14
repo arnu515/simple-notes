666557f1c22478ef87e7cca0a4c97072c4a824a3
arnu515 <arnu5152@gmail.com>
2021-03-14 18:34:01 +0530

    Add content creators

---
+++ src/components/content/Markdown.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { TextArea } from "carbon-components-svelte";
+  export let content: string;
+  export let id: string = "3-content";
+</script>
+
+<TextArea
+  labelText="Content"
+  bind:value={content}
+  {id}
+  rows={15}
+  hideLabel
+  placeholder="Enter some markdown here!" />
+<p>
+  <small>Learn how to style your content <a href="/docs/markdown">here</a></small>
+</p>
---
+++ src/components/content/Wysiwyg.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let content: string;
+  export let id: string = "3-content";
+
+  onMount(() => {
+    window["tinymce"].init({
+      selector: "#" + id,
+      menubar: false,
+      toolbar:
+        "undo redo | styleselect fontsizeselect | hr anchor charmap emoticons | forecolor backcolor | numlist bullist alignleft aligncenter alignright alignjustify | image media table | help",
+      plugins:
+        "advlist lists anchor autolink charmap emoticons help hr image media table",
+      image_uploadtab: false,
+      setup: (editor: any) => {
+        if (content) editor.setContent(content);
+        editor.on("change keyup compositionend setcontent", () => {
+          console.log(editor.getContent({ format: "html" }));
+          content = editor.getContent({ format: "html" });
+        });
+      },
+    });
+  });
+</script>
+
+<textarea {id} rows={15} />
